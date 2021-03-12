b0431209e46cf6441f553c5e29d6282191ac2293
Rabi Kumar <rabi-kumar@hotmail.com>
2021-03-12 23:04:14 +0800

    Added ios, android & web firebase support

--- lib/pages/appointment/appt_page.dart
+++ lib/pages/appointment/appt_page.dart
@@ -1,5 +1,6 @@
+import 'package:hair_salon/global_items/hairstylist_card.dart';
 import 'package:hair_salon/global_items/package_export.dart';
-import 'file:///C:/src/Flutter%20Projects/HairTech/hair_salon/lib/global_items/hairstylist_card.dart';
+
 import 'package:intl/intl.dart';
 
 class ApptPage extends StatefulWidget {
--- lib/varuns/database_service.dart
+++ lib/varuns/database_service.dart
@@ -1,58 +1,58 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:saludsingapore/Models/models.dart';
-import 'package:saludsingapore/Models/postage.dart';
-
-Stream<List<Postage>> postage() {
-  return PostageCollection().collection.snapshots().map((snapshot) {
-    return snapshot.docs.map((doc) => Postage.fromDocument(doc)).toList();
-  });
-}
-
-Stream<List<Products>> products() {
-  return ProductCollection().collection.snapshots().map((snapshot) {
-    return snapshot.docs.map((doc) => Products.fromDocument(doc)).toList();
-  });
-}
-
-Stream<List<Reviews>> reviews() {
-  return ReviewsCollection().collection.snapshots().map((snapshot) {
-    return snapshot.docs.map((doc) => Reviews.fromDocument(doc)).toList();
-  });
-}
-
-Stream<List<Categories>> categories() {
-  return CategoriesCollection().collection.snapshots().map((snapshot) {
-    return snapshot.docs.map((doc) => Categories.fromDocument(doc)).toList();
-  });
-}
-
-Stream<List<Order>> order() {
-  return OrderCollection().collection.snapshots().map((snapshot) {
-    return snapshot.docs.map((doc) => Order.fromDocument(doc)).toList();
-  });
-}
-
-class PostageCollection {
-  CollectionReference collection =
-      FirebaseFirestore.instance.collection('postage');
-}
-
-class ProductCollection {
-  CollectionReference collection =
-      FirebaseFirestore.instance.collection('products');
-}
-
-class OrderCollection {
-  CollectionReference collection =
-      FirebaseFirestore.instance.collection('order');
-}
-
-class CategoriesCollection {
-  CollectionReference collection =
-      FirebaseFirestore.instance.collection('categories');
-}
-
-class ReviewsCollection {
-  CollectionReference collection =
-      FirebaseFirestore.instance.collection('reviews');
-}
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:saludsingapore/Models/models.dart';
+// import 'package:saludsingapore/Models/postage.dart';
+//
+// Stream<List<Postage>> postage() {
+//   return PostageCollection().collection.snapshots().map((snapshot) {
+//     return snapshot.docs.map((doc) => Postage.fromDocument(doc)).toList();
+//   });
+// }
+//
+// Stream<List<Products>> products() {
+//   return ProductCollection().collection.snapshots().map((snapshot) {
+//     return snapshot.docs.map((doc) => Products.fromDocument(doc)).toList();
+//   });
+// }
+//
+// Stream<List<Reviews>> reviews() {
+//   return ReviewsCollection().collection.snapshots().map((snapshot) {
+//     return snapshot.docs.map((doc) => Reviews.fromDocument(doc)).toList();
+//   });
+// }
+//
+// Stream<List<Categories>> categories() {
+//   return CategoriesCollection().collection.snapshots().map((snapshot) {
+//     return snapshot.docs.map((doc) => Categories.fromDocument(doc)).toList();
+//   });
+// }
+//
+// Stream<List<Order>> order() {
+//   return OrderCollection().collection.snapshots().map((snapshot) {
+//     return snapshot.docs.map((doc) => Order.fromDocument(doc)).toList();
+//   });
+// }
+//
+// class PostageCollection {
+//   CollectionReference collection =
+//       FirebaseFirestore.instance.collection('postage');
+// }
+//
+// class ProductCollection {
+//   CollectionReference collection =
+//       FirebaseFirestore.instance.collection('products');
+// }
+//
+// class OrderCollection {
+//   CollectionReference collection =
+//       FirebaseFirestore.instance.collection('order');
+// }
+//
+// class CategoriesCollection {
+//   CollectionReference collection =
+//       FirebaseFirestore.instance.collection('categories');
+// }
+//
+// class ReviewsCollection {
+//   CollectionReference collection =
+//       FirebaseFirestore.instance.collection('reviews');
+// }
