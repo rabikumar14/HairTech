e4632b37e1a615dc2e0c49223220883dd604ce6f
varunbhalerao56 <varunbhalerao5902@gmail.com>
2021-03-20 21:04:42 +0800

    added payment backend along and included stripe sdk

---
+++ functions/src/handlers/confirmPaymentHandler.ts
@@ -0,0 +1,23 @@
+import * as functions from "firebase-functions";
+import {admin, adminVisibilityForState, clientVisibilityForState, intentToStatus, stripe} from "../utils";
+import paymentIntents from "stripe";
+
+export async function confirmPaymentHandler(data: any, context: functions.https.CallableContext) {
+    console.log(data);
+    const reservation = await admin.firestore().collection('reservations').doc(data.reservationId).get();
+    const paymentIntentId = reservation.get('paymentIntent');
+    const paymentData: paymentIntents.PaymentIntentConfirmParams = {};
+    paymentData.return_url = 'stripesdk://3ds.stripesdk.io';
+    if ('paymentMethodId' in data) paymentData.payment_method = data.paymentMethodId
+    // todo: confirm on client first
+    const intent = await stripe.paymentIntents.confirm(
+        paymentIntentId,
+        paymentData);
+    const status = intentToStatus(intent);
+    await reservation.ref.update({
+        state: status,
+        visibleInApp: clientVisibilityForState(status),
+        visibleInAdmin: adminVisibilityForState(status)
+    });
+    return {status: intent.status, clientSecret: intent.client_secret}
+}
